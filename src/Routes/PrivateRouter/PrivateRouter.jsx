76c3ddd5107cf0635f8b3e9c3892604ab9c9ff43
sajib37 <sajibhasan9811@gmail.com>
2023-12-22 02:11:48 +0600

    project created successfully

---
+++ src/Routes/PrivateRouter/PrivateRouter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivateRouter = () => {
+    return (
+        <div>
+            <h1>Privet Router</h1>
+        </div>
+    );
+};
+
+export default PrivateRouter;
